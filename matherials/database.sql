478bafed7a2e0c1f1b35f532b00b7101eaf0c6b3
dvolobuev116 <deevolt@yandex.ru>
2014-03-18 10:24:13 +0600

    Добавлены таблицы: специальность, группа, студент

--- matherials/database.sql
+++ matherials/database.sql
@@ -6,12 +6,35 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fio` VARCHAR(100) NOT NULL,
+  `photo` VARCHAR(200) NULL,
+  `prof_interest` TEXT NULL,
+  `projects` VARCHAR(300) NULL,
+  `languages` VARCHAR(200) NULL,
+  `begin_date` DATE NOT NULL,
+  `email` VARCHAR(100) NULL,
+  `phone` VARCHAR(100) NULL,
+  `degree` VARCHAR(100) NOT NULL,
+  `lecturer` TINYINT(1) NOT NULL,
+  `position` VARCHAR(100) NOT NULL,
+  `training` VARCHAR(200) NULL,
+  `consult_time` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Disciplines`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Disciplines` (
   `id_disciplines` INT NOT NULL AUTO_INCREMENT,
   `disciplines_name` VARCHAR(50) NOT NULL,
-  `description` TEXT NULL,
+  `description` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id_disciplines`))
 ENGINE = InnoDB;
 
@@ -25,32 +48,44 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Event` (
   `date_publication` DATE NOT NULL,
   `hold_date` DATE NOT NULL,
   `name_event` VARCHAR(100) NOT NULL,
-  `text_description` VARCHAR(45) NULL,
-  `url_pictures` VARCHAR(200) NULL,
+  `text_description` VARCHAR(45) NULL DEFAULT NULL,
+  `url_pictures` VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (`id_event`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Employee`
+-- Table `mydb`.`Specialization`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
+CREATE TABLE IF NOT EXISTS `mydb`.`Specialization` (
+  `number` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE INDEX `id_UNIQUE` (`number` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `fio` VARCHAR(100) NOT NULL,
-  `photo` VARCHAR(200) NULL DEFAULT NULL,
-  `prof_interest` TEXT NULL DEFAULT NULL,
-  `projects` VARCHAR(300) NULL DEFAULT NULL,
-  `languages` VARCHAR(200) NULL DEFAULT NULL,
-  `begin_date` DATE NOT NULL,
-  `email` VARCHAR(100) NULL DEFAULT NULL,
-  `phone` VARCHAR(100) NULL DEFAULT NULL,
-  `degree` VARCHAR(100) NOT NULL,
-  `lecturer` TINYINT(1) NOT NULL,
-  `position` VARCHAR(100) NOT NULL,
-  `training` VARCHAR(200) NULL DEFAULT NULL,
-  `consult_time` VARCHAR(200) NULL DEFAULT NULL,
+  `group` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Group`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Group` (
+  `number` VARCHAR(100) NOT NULL,
+  `specialization` VARCHAR(100) NOT NULL,
+  `course` INT NOT NULL,
+  PRIMARY KEY (`number`),
+  UNIQUE INDEX `number_UNIQUE` (`number` ASC))
 ENGINE = InnoDB;
 
 
