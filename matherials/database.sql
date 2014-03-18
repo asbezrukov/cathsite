bb1597103926d73e2d5516a0216b88281bca88a9
user <gorgonxxi@ya.ru>
2014-03-18 12:59:18 +0600

    Добавлены таблицы: отчет по практике, файл, курсовые работы

--- matherials/database.sql
+++ matherials/database.sql
@@ -6,28 +6,56 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Disciplines`
+-- Table `mydb`.`Files`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Disciplines` (
-  `id_disciplines` INT NOT NULL AUTO_INCREMENT,
-  `disciplines_name` VARCHAR(50) NOT NULL,
-  `description` TEXT NULL,
-  PRIMARY KEY (`id_disciplines`))
+CREATE TABLE IF NOT EXISTS `mydb`.`Files` (
+  `id_file` INT NOT NULL AUTO_INCREMENT,
+  `filename` VARCHAR(100) NOT NULL,
+  `loaddate` DATE NOT NULL,
+  PRIMARY KEY (`id_file`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Event`
+-- Table `mydb`.`PracticeReports`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Event` (
-  `id_event` INT NOT NULL AUTO_INCREMENT,
-  `category` VARCHAR(100) NOT NULL,
-  `date_publication` DATE NOT NULL,
-  `hold_date` DATE NOT NULL,
-  `name_event` VARCHAR(100) NOT NULL,
-  `text_description` VARCHAR(45) NULL,
-  `url_pictures` VARCHAR(200) NULL,
-  PRIMARY KEY (`id_event`))
+CREATE TABLE IF NOT EXISTS `mydb`.`PracticeReports` (
+  `id_preport` INT NOT NULL AUTO_INCREMENT,
+  `designrules` VARCHAR(500) NOT NULL,
+  `applform` VARCHAR(100) NOT NULL,
+  `practplaces` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id_preport`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CourseApplication`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CourseApplication` (
+  `id_cappl` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `decription` TEXT NULL,
+  `id_lecturer` INT NOT NULL,
+  `literature` VARCHAR(150) NULL,
+  `id_file` INT NOT NULL,
+  PRIMARY KEY (`id_cappl`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CourseThemes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CourseThemes` (
+  `id_theme` INT NOT NULL AUTO_INCREMENT,
+  `number` INT NOT NULL,
+  `year` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL,
+  `id_lecturer` INT NOT NULL,
+  `literature` VARCHAR(150) NOT NULL,
+  `id_file` INT NOT NULL,
+  PRIMARY KEY (`id_theme`),
+  UNIQUE INDEX `idcrp_UNIQUE` (`id_theme` ASC))
 ENGINE = InnoDB;
 
 
@@ -51,6 +79,32 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
   `consult_time` VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Disciplines`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Disciplines` (
+  `id_disciplines` INT NOT NULL AUTO_INCREMENT,
+  `disciplines_name` VARCHAR(50) NOT NULL,
+  `description` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_disciplines`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Event`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Event` (
+  `id_event` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(100) NOT NULL,
+  `date_publication` DATE NOT NULL,
+  `hold_date` DATE NOT NULL,
+  `name_event` VARCHAR(100) NOT NULL,
+  `text_description` VARCHAR(45) NULL DEFAULT NULL,
+  `url_pictures` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_event`))
 ENGINE = InnoDB;
 
 
@@ -116,18 +170,15 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`News`
+-- Table `mydb`.`CoursePapers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`News` (
-  `id_news` INT NOT NULL AUTO_INCREMENT,
-  `category` VARCHAR(100) NOT NULL,
-  `date_publication` DATE NOT NULL,
-  `news_pictures` VARCHAR(200) NULL,
-  `publication_main` TINYINT(1) NOT NULL,
-  `header` VARCHAR(100) NOT NULL,
-  `preview` TEXT NOT NULL,
-  `text_description` TEXT NOT NULL,
-  PRIMARY KEY (`id_news`))
+CREATE TABLE IF NOT EXISTS `mydb`.`CoursePapers` (
+  `id_cp` INT NOT NULL AUTO_INCREMENT,
+  `designrules` VARCHAR(200) NULL,
+  `idcappl` INT NOT NULL,
+  `id_theme` INT NOT NULL,
+  `approved` TINYINT(1) NULL,
+  PRIMARY KEY (`id_cp`))
 ENGINE = InnoDB;
 
 
@@ -141,6 +192,22 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Classifieds` (
   `text` TEXT NOT NULL,
   `important` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id_сlassifieds`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`News`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`News` (
+  `id_news` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(100) NOT NULL,
+  `date_publication` DATE NOT NULL,
+  `news_pictures` VARCHAR(200) NULL DEFAULT NULL,
+  `publication_main` TINYINT(1) NOT NULL,
+  `header` VARCHAR(100) NOT NULL,
+  `preview` TEXT NOT NULL,
+  `text_description` TEXT NOT NULL,
+  PRIMARY KEY (`id_news`))
 ENGINE = InnoDB;
 
 
