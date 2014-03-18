6cd60e734f8501f6b954073fdffd37214efa83ad
Kolodkin Artem <TKTier@yandex.ru>
2014-03-18 11:54:42 +0600

    Добавлена таблица: новости.

--- matherials/database.sql
+++ matherials/database.sql
@@ -6,35 +6,12 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Employee`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `fio` VARCHAR(100) NOT NULL,
-  `photo` VARCHAR(200) NULL,
-  `prof_interest` TEXT NULL,
-  `projects` VARCHAR(300) NULL,
-  `languages` VARCHAR(200) NULL,
-  `begin_date` DATE NOT NULL,
-  `email` VARCHAR(100) NULL,
-  `phone` VARCHAR(100) NULL,
-  `degree` VARCHAR(100) NOT NULL,
-  `lecturer` TINYINT(1) NOT NULL,
-  `position` VARCHAR(100) NOT NULL,
-  `training` VARCHAR(200) NULL,
-  `consult_time` VARCHAR(200) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `mydb`.`Disciplines`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Disciplines` (
   `id_disciplines` INT NOT NULL AUTO_INCREMENT,
   `disciplines_name` VARCHAR(50) NOT NULL,
-  `description` TEXT NULL DEFAULT NULL,
+  `description` TEXT NULL,
   PRIMARY KEY (`id_disciplines`))
 ENGINE = InnoDB;
 
@@ -48,9 +25,32 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Event` (
   `date_publication` DATE NOT NULL,
   `hold_date` DATE NOT NULL,
   `name_event` VARCHAR(100) NOT NULL,
-  `text_description` VARCHAR(45) NULL DEFAULT NULL,
-  `url_pictures` VARCHAR(200) NULL DEFAULT NULL,
+  `text_description` VARCHAR(45) NULL,
+  `url_pictures` VARCHAR(200) NULL,
   PRIMARY KEY (`id_event`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fio` VARCHAR(100) NOT NULL,
+  `photo` VARCHAR(200) NULL DEFAULT NULL,
+  `prof_interest` TEXT NULL DEFAULT NULL,
+  `projects` VARCHAR(300) NULL DEFAULT NULL,
+  `languages` VARCHAR(200) NULL DEFAULT NULL,
+  `begin_date` DATE NOT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  `phone` VARCHAR(100) NULL DEFAULT NULL,
+  `degree` VARCHAR(100) NOT NULL,
+  `lecturer` TINYINT(1) NOT NULL,
+  `position` VARCHAR(100) NOT NULL,
+  `training` VARCHAR(200) NULL DEFAULT NULL,
+  `consult_time` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB;
 
 
@@ -96,8 +96,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Laboratory` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NOT NULL,
   `address` VARCHAR(200) NOT NULL,
-  `description` VARCHAR(300) NULL,
-  `photo` VARCHAR(300) NULL,
+  `description` VARCHAR(300) NULL DEFAULT NULL,
+  `photo` VARCHAR(300) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB;
@@ -112,6 +112,22 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Laboratory-Employee` (
   `id_laboratory` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`News`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`News` (
+  `id_news` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(100) NOT NULL,
+  `date_publication` DATE NOT NULL,
+  `news_pictures` VARCHAR(200) NULL,
+  `publication_main` TINYINT(1) NOT NULL,
+  `header` VARCHAR(100) NOT NULL,
+  `preview` TEXT NOT NULL,
+  `text_description` TEXT NOT NULL,
+  PRIMARY KEY (`id_news`))
 ENGINE = InnoDB;
 
 
