09affc3e8f8853d2e2d9ce1fdc07f43ad9a7b299
dvolobuev116 <deevolt@yandex.ru>
2014-03-18 16:59:03 +0600

    Добавлены внешние ключи + фиксы.

--- matherials/database.sql
+++ matherials/database.sql
@@ -2,62 +2,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+DROP SCHEMA IF EXISTS `mydb` ;
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`Files`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Files` (
-  `id_file` INT NOT NULL AUTO_INCREMENT,
-  `filename` VARCHAR(100) NOT NULL,
-  `loaddate` DATE NOT NULL,
-  PRIMARY KEY (`id_file`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`PracticeReports`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PracticeReports` (
-  `id_preport` INT NOT NULL AUTO_INCREMENT,
-  `designrules` VARCHAR(500) NOT NULL,
-  `applform` VARCHAR(100) NOT NULL,
-  `practplaces` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`id_preport`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`CourseApplication`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CourseApplication` (
-  `id_cappl` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `decription` TEXT NULL,
-  `id_lecturer` INT NOT NULL,
-  `literature` VARCHAR(150) NULL,
-  `id_file` INT NOT NULL,
-  PRIMARY KEY (`id_cappl`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`CourseThemes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CourseThemes` (
-  `id_theme` INT NOT NULL AUTO_INCREMENT,
-  `number` INT NOT NULL,
-  `year` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `description` TEXT NULL,
-  `id_lecturer` INT NOT NULL,
-  `literature` VARCHAR(150) NOT NULL,
-  `id_file` INT NOT NULL,
-  PRIMARY KEY (`id_theme`),
-  UNIQUE INDEX `idcrp_UNIQUE` (`id_theme` ASC))
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Employee`
@@ -165,33 +112,19 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Laboratory-Employee` (
   `id_employee` INT NOT NULL,
   `id_laboratory` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`CoursePapers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CoursePapers` (
-  `id_cp` INT NOT NULL AUTO_INCREMENT,
-  `designrules` VARCHAR(200) NULL,
-  `idcappl` INT NOT NULL,
-  `id_theme` INT NOT NULL,
-  `approved` TINYINT(1) NULL,
-  PRIMARY KEY (`id_cp`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Classifieds`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Classifieds` (
-  `id_сlassifieds` INT NOT NULL AUTO_INCREMENT,
-  `date_publication` DATE NOT NULL,
-  `header` VARCHAR(100) NOT NULL,
-  `text` TEXT NOT NULL,
-  `important` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id_сlassifieds`))
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_id_employee_idx` (`id_employee` ASC),
+  INDEX `kf_id_laboratory_idx` (`id_laboratory` ASC),
+  CONSTRAINT `fk_id_employee`
+    FOREIGN KEY (`id_employee`)
+    REFERENCES `mydb`.`Employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `kf_id_laboratory`
+    FOREIGN KEY (`id_laboratory`)
+    REFERENCES `mydb`.`Laboratory` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -212,6 +145,19 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Classifieds`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Classifieds` (
+  `id_сlassifieds` INT NOT NULL AUTO_INCREMENT,
+  `date_publication` DATE NOT NULL,
+  `header` VARCHAR(100) NOT NULL,
+  `text` TEXT NOT NULL,
+  `important` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`id_сlassifieds`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Subscribers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Subscribers` (
@@ -219,6 +165,115 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Subscribers` (
   `name` VARCHAR(50) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id_subscribers`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Files`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Files` (
+  `id_file` INT NOT NULL AUTO_INCREMENT,
+  `filename` VARCHAR(100) NOT NULL,
+  `loaddate` DATE NOT NULL,
+  PRIMARY KEY (`id_file`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`PracticeReports`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`PracticeReports` (
+  `id_preport` INT NOT NULL AUTO_INCREMENT,
+  `designrules` VARCHAR(500) NOT NULL,
+  `applform` VARCHAR(100) NOT NULL,
+  `practplaces` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id_preport`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CourseApplication`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CourseApplication` (
+  `id_cappl` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `decription` TEXT NULL DEFAULT NULL,
+  `id_lecturer` INT NOT NULL,
+  `literature` VARCHAR(300) NULL DEFAULT NULL,
+  `id_file` INT NOT NULL,
+  PRIMARY KEY (`id_cappl`),
+  INDEX `fk_id_lecturer_idx` (`id_lecturer` ASC),
+  INDEX `fk_id_file_idx` (`id_file` ASC),
+  CONSTRAINT `fk_id_lecturer`
+    FOREIGN KEY (`id_lecturer`)
+    REFERENCES `mydb`.`Employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_file`
+    FOREIGN KEY (`id_file`)
+    REFERENCES `mydb`.`Files` (`id_file`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CourseThemes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CourseThemes` (
+  `id_theme` INT NOT NULL AUTO_INCREMENT,
+  `spec_number` VARCHAR(100) NOT NULL,
+  `year` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL DEFAULT NULL,
+  `id_lecturer` INT NOT NULL,
+  `literature` VARCHAR(150) NOT NULL,
+  `id_file` INT NOT NULL,
+  PRIMARY KEY (`id_theme`),
+  UNIQUE INDEX `idcrp_UNIQUE` (`id_theme` ASC),
+  INDEX `fk_id_lecturer_idx` (`id_lecturer` ASC),
+  INDEX `fk_id_file_idx` (`id_file` ASC),
+  INDEX `fk_spec_number_idx` (`spec_number` ASC),
+  CONSTRAINT `fk_id_lecturer`
+    FOREIGN KEY (`id_lecturer`)
+    REFERENCES `mydb`.`Employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_file`
+    FOREIGN KEY (`id_file`)
+    REFERENCES `mydb`.`Files` (`id_file`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_spec_number`
+    FOREIGN KEY (`spec_number`)
+    REFERENCES `mydb`.`Specialization` (`number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CoursePapers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CoursePapers` (
+  `id_cp` INT NOT NULL AUTO_INCREMENT,
+  `designrules` VARCHAR(300) NULL DEFAULT NULL,
+  `id_cappl` INT NOT NULL,
+  `id_theme` INT NOT NULL,
+  `approved` VARCHAR(300) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_cp`),
+  INDEX `fk_id_capple_idx` (`id_cappl` ASC),
+  INDEX `fk_id_theme_idx` (`id_theme` ASC),
+  CONSTRAINT `fk_id_capple`
+    FOREIGN KEY (`id_cappl`)
+    REFERENCES `mydb`.`CourseApplication` (`id_cappl`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_theme`
+    FOREIGN KEY (`id_theme`)
+    REFERENCES `mydb`.`CourseThemes` (`id_theme`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
