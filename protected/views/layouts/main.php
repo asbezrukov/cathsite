3ee957e3a90c2c32af330e2be4ce22867a437bd6
SVKiziyarov <sergej93@mail.ru>
2014-03-20 00:13:13 +0500

    head.title

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -7,7 +7,7 @@
 <!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-    <title>Universe - College Education Responsive Template</title>
+    <title>ИМиКН</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="College Education Responsive Template">
     <meta name="author" content="Esmet">
