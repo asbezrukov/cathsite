92e0c68dcb1df1822283ac042a6c1b717ab9348b
vpodryvanov116 <slava-vargashi@mail.ru>
2014-03-21 17:38:44 +0600

    Добавлены методы модели SubscribersModel

--- protected/models/SubscribersModel.php
+++ protected/models/SubscribersModel.php
@@ -1,5 +1,18 @@
 <?php
 SubscribersModel extends CActiveRecord
 {
-
+       public function tableName() {
+        return 'subscribers';
+    }
+    
+    public function rules() {
+        
+        return array(
+            
+        );
+    }
+    
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    } 
 }
