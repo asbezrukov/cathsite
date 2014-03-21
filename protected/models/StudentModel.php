3808b2a4f44cfc63af9468136a3ca0a126b4512d
vpodryvanov116 <slava-vargashi@mail.ru>
2014-03-21 17:35:05 +0600

    Добавлены методы модели StudentModel

--- protected/models/StudentModel.php
+++ protected/models/StudentModel.php
@@ -1,5 +1,18 @@
 <?php
 StudentModel extends CActiveRecord
 {
+       public function tableName() {
+        return 'student';
+    }
     
+    public function rules() {
+        
+        return array(
+            
+        );
+    }
+    
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    } 
 }
