d5d79b96e2e169d14162b6fc366adaa805293561
vpodryvanov116 <slava-vargashi@mail.ru>
2014-03-21 17:25:22 +0600

    Добавлены методы модели EmployeeModel

--- protected/models/EmployeeModel.php
+++ protected/models/EmployeeModel.php
@@ -2,5 +2,20 @@
 
 class EmployeeModel extends CActiveRecord
 {
-
+    
+    public function tableName() {
+        return 'employee';
+    }
+    
+    public function rules() {
+        
+        return array(
+            
+        );
+    }
+    
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
+    
 }
