0ae0cc466a0ff0e67ab3e099673cc7e24d0c69ed
SFomin116 <sergei.fomin@list.ru>
2014-03-24 01:21:57 +0600

    Добавлен класс модели 'DisciplinesModel'

---
+++ protected/models/DisciplinesModel.php
@@ -0,0 +1,39 @@
+<?php
+
+class DisciplinesModel extends CActiveRecord {
+
+    //Здесь храняться данные полученные из массива $_POST в контрллере
+    public $tempData = array();
+
+    public function tableName() {
+        return 'disciplines';
+    }
+
+    public function rules() {
+
+        return array(
+
+        );
+    }
+
+    /**
+     *  Метод выполняется после вызова метода модели save(),
+     *  В нем будут записываться данные из переменной tempData в модель, и проверка на валидность.
+     */
+    public function beforeSave() {
+
+        if ($this->validate())
+            return true;
+        else
+            return false;
+    }
+
+    public function afterSave() {
+        unset($this->tempData);
+    }
+
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
+
+}
