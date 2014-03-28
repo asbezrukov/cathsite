6e934c08832cde48d30af40747dca306afbfb7b2
Evgenij Todyshev <evgenijtodyshev@mail.ru>
2014-03-28 14:58:08 +0600

    Добавленна модель LaboratoryEmployee

---
+++ protected/models/LaboratoryEmployee.php
@@ -0,0 +1,39 @@
+<?php
+
+class LaboratoryEmployee extends CActiveRecord
+{
+    //«десь хран€тьс€ данные полученные из массива $_POST в контрллере
+    public $tempData = array();
+
+    public function tableName() {
+        return 'laboratory-employee';
+    }
+    
+    public function rules() {
+        
+        return array(
+
+        );
+    }
+
+    /**
+     *  ћетод выполн€етс€ после вызова метода модели save(),
+     *  ¬ нем будут записыватьс€ данные из переменной tempData в модель, и проверка на валидность.
+     */
+    public function beforeSave() {
+
+        if ($this->validate())
+            return true;
+        else
+            return false;
+    }
+
+    public function afterSave() {
+        unset($this->tempData);
+    }
+
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
+    
+}
