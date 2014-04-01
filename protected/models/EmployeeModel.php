71c425483b4e709f96e66caadb465e25e2ae0979
Karine <karine_666@mail.ru>
2014-04-01 12:11:04 +0600

    в ветке issue_employee в в модели cathsite / protected / models / EmployeeModel.php  добавлены public function photo - выборка 3 случайных сотрудников,имеющих фото, и public function sortFIO - выборка списка сотрудников, сортировка по фамилии.

--- protected/models/EmployeeModel.php
+++ protected/models/EmployeeModel.php
@@ -3,6 +3,30 @@
 class EmployeeModel extends CActiveRecord
 {
 
+
+ public $tempData = array();
+ public $image;
+	
+	public function photo($limit=3)
+	{
+		$criteria=new CDbCriteria;
+		$criteria->select='id, fio';
+		$criteria->condition='photo != null';
+        $criteria->order='id rand()';
+		$criteria-> limit=$limit;		
+		return $this->findAll($criteria);
+	}
+
+     public function sortFIO()
+	{
+		$criteria=new CDbCriteria;
+		$criteria->select='id, fio';
+		$criteria->order='fio DESC';
+	    return $this->findAll($criteria);
+		
+	}
+
+
     public function tableName() {
         return 'Employee';
     }
@@ -10,10 +34,26 @@ class EmployeeModel extends CActiveRecord
     public function rules() {
         
         return array(
+array('image', 'file', 'types'=>'jpg, gif, png'),
+
             
         );
+    }   
+
+
+    public function beforeSave() {
+
+        if ($this->validate())
+            return true;
+        else
+            return false;
     }
-    
+
+    public function afterSave() {
+        unset($this->tempData);
+    }
+
+
     public static function model($className=__CLASS__) {
         return parent::model($className);
     }
