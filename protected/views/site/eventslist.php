881316d4b2698ddd4a85a7ce6b8cffebfd01c628
SVKiziyarov <sergej93@mail.ru>
2014-04-01 09:26:16 +0500

    Представление для вывода 20 сотрудников

---
+++ protected/views/site/eventslist.php
@@ -0,0 +1,336 @@
+    <!-- Being Page Title -->
+    <div class="container">
+        <div class="page-title clearfix">
+            <div class="row">
+                <div class="col-md-12">
+                    <h6><a href="?r=site/index">Главная</a></h6>
+                    <h6><span class="page-active">Преподаватели</span></h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+
+            <!-- Here begin Main Content -->
+            <div class="col-md-8">
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                    <div class="col-md-12">
+                        <div class="list-event-item">
+                            <div class="box-content-inner clearfix">
+                                <div class="list-event-thumb">
+                                    <a href="event-single.html">
+                                        <img src="http://placehold.it/170x150" alt="">
+                                    </a>
+                                </div>
+                                <h5 class="event-title"><a href="event-single.html">ФИО</a></h5>
+                                <p>звание, степень, должность</p><br>
+                                <p>время приключений на кафедре</p>
+                            </div> <!-- /.box-content-inner -->
+                        </div> <!-- /.list-event-item -->
+                    </div> <!-- /.col-md-12 -->
+
+                </div> <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="load-more-btn">
+                            <a href="#">Click here to load more events</a>
+                        </div>
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+
+            </div> <!-- /.col-md-8 -->
+
+            <!-- Here begin Sidebar -->
+            
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
