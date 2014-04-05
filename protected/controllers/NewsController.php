46a142dca94deacceaa33742bdb9d1b7f09997e8
Timur Teregulov <teregulov.t.r@gmail.com>
2014-04-05 10:42:00 +0600

    Изменён NewsController

--- protected/controllers/NewsController.php
+++ protected/controllers/NewsController.php
@@ -4,10 +4,35 @@ class NewsController extends Controller
 {
 	public function actionIndex()
 	{
-		$this->render('Index');
+		$model = new NewsModel();
+		$arResult['recently'] = $model->recently(3);
+	
+	
+		$this->render('index', array('arResult'=>$arResult));
 	}
 	
-	public function actionDetail($id)
+	public function actionList()
+	{
+	$criteria = new CDbCriteria();
+	$count = NewsModel::model()->count($criteria);
+		$pages = new CPagination($count); 
+		$pages->pageSize = 10;
+        $pages->applyLimit($criteria);
+
+        $arResult['pages'] = $pages;
+        $data = $model->recently();
+        $dataProvider = new CActiveDataProvider($model);
+        $dataProvider->setData($data);
+		$arResult['dataProvider'] = $dataProvider;
+        $this->render('list', array('arResult'=>$arResult));
+	}
+	
+	    public function actionGrid() 
+	{
+	
+    }
+	
+	public function actionDetail()
 	{
 		$this->render('Detail');	
 	}
