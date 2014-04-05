c091001c8bd9f6fa691172c908d05cfdab5f0061
dvolobuev116 <deevolt@yandex.ru>
2014-04-06 00:11:35 +0600

    Внесены правки в NewsModel

--- protected/models/NewsModel.php
+++ protected/models/NewsModel.php
@@ -1,41 +1,17 @@
 <?php
 
-class NewsModel extends CActiveRecord 
+class NewsModel extends CActiveRecord
 {
+    public $tempData = array();
 	public $image;
-	public $tempData = array();
-	
-	public function last_news($limit = 3)
+
+	public function last_news($limit=3)
 	{
-		
 		$criteria = new CDbCriteria;
 		$criteria->select = 'id_news, header, date_publication, preview, text_description';
 		$criteria->order = 'date_publication DESC';
 		$criteria->limit = $limit;
-		$this->setDbCriteria($criteria);
 		return $this->findAll($criteria);
-		
-		
-		/* $news = new CActiveDataProvider('News', array(
-			'select' => 'id_news, header, date_publication, preview, text_description',
-			'criteria' => array(                    
-				'order' => "t.date_publication DESC",
-			),
-			'limit' => $limit,
-			'pagination' => FALSE
-        ));
-		return $news->GetData();*/
-	}
-	
-	
-	public function tableName()
-	{
-		return 'News';
-	}
-	
-	public function rules()
-	{
-		return array('image', 'file', 'types'=>'jpg, gif, png'),
 	}
 	
 	public function relations()
@@ -44,38 +20,32 @@ class NewsModel extends CActiveRecord
 			'iscategory' =>array(self::BELONGS_TO, 'NewsCategory', 'category')
 		);
 	}
-	/*
-	* чтобы получить категорию for controller
-	*
-	* foreach ($news as $val) // перебираем все категории
-	* {
-	* 		$category = $val->iscategory;
-	* }
-	*/
 	
-	public function attributeLabels()
-	{
-		return array();
-	}
-	
-	public function search()
-	{	
-	}
-		
-	public function beforeSave() {
-
-		if ($this->validate())
-			return true;
-	else
-		return false;
+    public function tableName() {
+        return 'News';
+    }
+    
+    public function rules() {
+        
+        return array(
+			array('image', 'file', 'types'=>'jpg, gif, png'),
+        );
     }
 
-	public function afterSave() {
-			unset($this->tempData);
+    public function beforeSave() {
+
+        if ($this->validate())
+            return true;
+        else
+            return false;
     }
-		
-	public static function model ($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+
+    public function afterSave() {
+        unset($this->tempData);
+    }
+
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
+    
 }
