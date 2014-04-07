4645e490b3877abc066710a0a917d4450b76e8d4
SVKiziyarov <sergej93@mail.ru>
2014-04-07 17:25:22 +0500

    eventsingle Представление для отображения одного сотрудника

---
+++ protected/views/site/pages/eventsingle.php
@@ -0,0 +1,56 @@
+﻿<!-- Being Page Title -->
+    <div class="container">
+        <div class="page-title clearfix">
+            <div class="row">
+                <div class="col-md-12">
+                    <h6><a href="?r=site/index">Главная</a></h6>
+                    <h6><a href="#">Преподаватели</a></h6>
+                    <h6><span class="page-active">Преподаватель</span></h6>
+					<div class="grid-or-list">
+                        <ul>
+                            <li><a href="#" title="Редактировать"><img src="/images/edit.png"></a></li>
+                            <li><a href="#" title="Удалить"><img src="/images/delete.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+    <div class="container">
+        <div class="row">
+
+            <!-- Here begin Main Content -->
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="event-container clearfix">
+                            <div class="left-event-content">
+                                <img src="http://placehold.it/225x240" alt="">
+                                <div class="event-contact">
+                                    <h4>Контакты</h4>
+                                    <ul>
+                                        <li>Название Кафедры</li>
+                                        <li>№ кабинета</li>
+                                        <li>Телефон</li>
+                                    </ul>
+                                </div>
+                            </div> <!-- /.left-event-content -->
+                            <div class="right-event-content">
+                                <h2 class="event-title">Ф.И.О.</h2> 
+                                <p>Звание, степень, должность</p>
+                                <p>"немного о себе"</p>
+                                <p>время консультаций</p>
+                                </div>
+                            </div> <!-- /.right-event-content -->
+                        </div> <!-- /.event-container -->
+                    </div>
+                </div> <!-- /.row -->
+            </div> <!-- /.col-md-8 -->
+
+            <!-- Here begin Sidebar -->
+            <div class="col-md-4">                
+            </div> <!-- /.col-md-4 -->
+    
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
