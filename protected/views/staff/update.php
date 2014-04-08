8840783eafdadf2ce7458f610e360e6987041c20
ekarpova116 <karpova_ekaterina_116@mail.ru>
2014-04-08 09:34:06 +0600

    Форма для добавления-редактирования сотрудников

---
+++ protected/views/staff/update.php
@@ -0,0 +1,115 @@
+    <!-- Being Page Title -->
+    <div class="container">
+        <div class="page-title clearfix">
+            <div class="row">
+                <div class="col-md-12">
+                    <h6><a href="/">Главная</a></h6>
+                    <h6><span class="page-active">Обновление данных</span></h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-page-content">
+                    <div class="contact-heading">
+                        <h3>Заполните форму</h3>
+                    </div>
+                    <div class="contact-form clearfix">
+                        <p class="full-row">
+                            <span class="contact-label">
+                                <label for="name-id">Фамилия:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Имя:</label>
+                            </span>
+                            <input type="text" id="name-id" name="name-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Отчество:</label>
+                            </span>
+                            <input type="text" id="patronymic-id" name="patronymic-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Профессиональные интересы:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Проекты:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Языки:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="email-id">E-mail:</label>
+                            </span>
+                            <input type="text" id="email-id" name="email-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Телефон:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Научная степень:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Преподаватель:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Имя:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Имя:</label>
+                            </span>
+                            <input type="text" id="surname-id" name="surname-id">
+                        </p>
+                        <p class="full-row"> 
+                            <span class="contact-label">
+                                <label for="surname-id">Имя:</label>
+                            </span>
+                            <input type="text" id="rank-id" name="rank-id">
+                        </p>
+                        <p class="full-row">
+                            <span class="contact-label">
+                                <label for="email-id">Дата трудоустройства:</label>
+                            </span>
+                            <input type="text" id="email-id" name="email-id">
+                        </p>
+                        
+                        <p class="full-row">
+                            <input class="mainBtn" type="submit" name="" value="Send Message">
+                        </p>
+                    </div>
+                </div>
+            </div> <!-- /.col-md-7 -->
+
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
