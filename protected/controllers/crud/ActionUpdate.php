60d1eb9d997c4c6d0b9f5db40de4d6b4255f0f31
vpodryvanov116 <slava-vargashi@mail.ru>
2014-04-13 11:53:27 +0600

    Добавлено действие контроллера ActionUpdate

---
+++ protected/controllers/crud/ActionUpdate.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ *  Действие обновляет запись в модели
+ *
+ *  Входные параметры:
+ *      $mid - название модели
+ *      $id - идентификатор записи
+ *
+ *  Пример вызова: ?r=<controllerid>/update&mid=news&id=102
+ *
+ **/
+class ActionUpdate extends CrudAction
+{
+    public function run($mid, $id)
+    {
+        //Действие работает аналогично действию 'actionCreate'
+        $postMid = $this->modelClassFormatter($mid);
+
+        $model=$this->loadModel($mid, $id);
+
+        if(isset($_POST[$postMid]))
+        {
+            $data = $_POST[$postMid];
+            //Метод save автоматически вызывает метод beforeSave модели, в котором записываются данные из переменной '$model->tempData' в модель, проходят валидацию и сохраняются.
+            //Метод beforeSave у каждой модели свой.
+            $model->tempData = $data;
+
+            if($model->save())
+                $this->controller->redirect(array('detail', 'id'=>$id));
+        }
+
+        $this->controller->render('crud/_form',array(
+            'model'=>$model,
+        ));
+    }
+}
+?>
