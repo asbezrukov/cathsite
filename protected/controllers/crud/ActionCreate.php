0974ee72e417ab1a3e2cafc1fb69a6370b38e835
vpodryvanov116 <slava-vargashi@mail.ru>
2014-04-13 11:52:35 +0600

    Добавлено действие контроллера ActionCreate

---
+++ protected/controllers/crud/ActionCreate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ *  Действие создает запись в модели
+ *
+ *  Входные параметры:
+ *      $mid - название модели
+ *
+ *  Пример вызова: ?r=<controllerid>/create&mid=users
+ *
+ **/
+class ActionCreate extends CrudAction
+{
+    public function run($mid) {
+        //Первый символ поднимаем в верхний регистр, т.к. название всех моделей начинается с большой буквы, для массива $_POST это имеет значения, в остальных случаях нет.
+        $postMid = $this->modelClassFormatter($mid);
+        //Создаем модель
+        $model=$this->loadModel($mid);
+        //Если данные отправлены, то записываем их и сохраняем в базе, иначе открываем форму создания записи.
+        if(isset($_POST[$postMid]))
+        {
+            $data = $_POST[$postMid];
+
+            $model->tempData = $data;
+            //Метод save автоматически вызывает метод beforeSave модели, в котором записываются данные из переменной '$model->tempData' в модель, проходят валидацию и сохраняются.
+            //Метод beforeSave у каждой модели свой.
+            if($model->save())
+                $this->controller->redirect(array('list'));
+        }
+
+        $this->controller->render('crud/_form',array(
+            'model'=>$model,
+        ));
+    }
+}
+?>
