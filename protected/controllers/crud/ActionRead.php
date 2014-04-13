46f0de610065cc37aa869370faab6ff28d197a5f
vpodryvanov116 <slava-vargashi@mail.ru>
2014-04-13 11:53:09 +0600

    Добавлено действие контроллера ActionRead

---
+++ protected/controllers/crud/ActionRead.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ *  Действие выводит запись модели по ее id.
+ *
+ *  Входные параметры:
+ *      $mid - название модели для которой нужно получить запись
+ *      $id - идентификатор записи
+ *
+ *  Пример вызова: ?r=<controllerid>/view&mid=news&id=8
+ *      crud/view  - контроллер/действие
+ *      mid=news  - таблица
+ *      id=8  - 8 запись в таблице news
+ *
+ **/
+class ActionRead extends CrudAction
+{
+    public function run($mid, $id)
+    {
+        $model = $this->loadModel($mid, $id);
+
+        $arResult['data'] = $model;
+
+        $this->controller->render('detail', array(
+            'arResult'=>$arResult,
+        ));
+    }
+}
+?>
