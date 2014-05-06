da40a89b4af6f611a6e567a7ca100315d3346a03
dvolobuev116 <deevolt@yandex.ru>
2014-05-06 11:08:07 +0600

    Добавлено действие show контролера page

--- protected/controllers/PageController.php
+++ protected/controllers/PageController.php
@@ -19,8 +19,17 @@ class PageController extends Controller
     {
 		$page=new PagesModel;
 		$page->title=$_POST["title"];
+		$page->p_name=$_POST["p_name"];
+		$page->category=$_POST["category"];
 		$page->content=$_POST["content"];
 		$page->save();
         $this->redirect('/');
     }
+	
+	public function actionShow($p_name)
+	{
+		$page = new PagesModel;
+		$page = PagesModel::model()->find(array('condition'=>'p_name = "'.$p_name.'"'));
+		$this->render('show', array('page'=>$page));
+	}
 }
