3d2443a2ff657df33d37bdf2b0e5a771dd0be733
SFomin116 <sergei.fomin@list.ru>
2014-05-25 10:53:17 +0600

    Добавлена подсказка на форме входа пользователя.

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -2,19 +2,35 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
-?>  
+?>
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-	        	<form name = "formLogin" action = "/site/login" method = "post">
-	        		<table class="formAuth">  
-	        			<tr> <td> Введите логин </td> <td> <input type="text" name="username" ></td> </tr>
-	        			<tr><td> </td></tr>
-	        			<tr> <td> Введите пароль</td> <td> <input type="password" name="password" ></td> </tr>
-	        			<tr><td> </td></tr>
-	        			<tr> <td> </td> <td> <input type="submit" value = "Войти"></td> </tr>
-		           </table>
-	            </form>
+            <div class="widget-main">
+                <div class="widget-inner">
+                    <form name = "formLogin" action = "/site/login" method = "post">
+                        <div class="form-auth-wrap">
+                            <table class="form-auth">
+                                <tr><td> Введите логин </td> <td> <input type="text" name="username" ></td> </tr>
+                                <tr><td><div class="form-auth-space"></div></td></tr>
+                                <tr><td> Введите пароль</td> <td> <input type="password" name="password" ></td> </tr>
+                                <tr><td><div class="form-auth-space"></div></td></tr>
+                                <tr><td></td><td><input type="submit" value = "Войти"></td> </tr>
+                           </table>
+                        </div>
+                    </form>
+                    <p class="hint">
+                        Учетные записи для входа:<br>
+                        Login: <span class="hint-light">admin</span><br>
+                        Login: <span class="hint-light">chiefstudent</span><br>
+                        Login: <span class="hint-light">chiefstaff</span><br>
+                        Login: <span class="hint-light">student</span><br>
+                        Login: <span class="hint-light">staff</span><br>
+                        Пароль для всех: <span class="hint-light">1</span>
+                    </p>
+                </div>
+            </div>
         </div> <!-- /.col-md-8 -->
     </div>
 </div>
