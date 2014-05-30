413d35b18c665a1d3913e2af577a270631088f37
Kolodkin Artem <TKTier@yandex.ru>
2014-05-30 11:49:02 +0600

    Внесены изменения в файл list_events.php

--- protected/views/admin/list_events.php
+++ protected/views/admin/list_events.php
@@ -1,12 +1,12 @@
-<div class="container">
+п»ї<div class="container">
     <div class="page-title clearfix">
         <div class="row">
             <div class="col-md-12">
-                <h6><a href="/">Главная</a></h6>
-                <h6><span>Редактирование</span></h6>
+                <h6><a href="/">Р“Р»Р°РІРЅР°СЏ</a></h6>
+                <h6><span>Р РµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ</span></h6>
                 <div class="grid-or-list">
                     <ul>
-                        <li><a href="/event/create" title="Добавить"><img src="/images/add.png"></a></li>
+                        <li><a href="/event/create" title="Р”РѕР±Р°РІРёС‚СЊ"><img src="/images/add.png"></a></li>
                     </ul>
                 </div>
             </div>
@@ -14,31 +14,34 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="widget-main">
-                <div class="widget-inner">
-                        <dl class="course-list" role="tablist">
-                            <?php foreach($arResult as $arItem) { ?>
-                                <dt>
-                                    <a class="fa fa-times pull-right" href="/event/delete/<? echo $arItem->id_event?>"></a>
-                                    <a class="fa fa-pencil pull-right" href="/event/update/<? echo $arItem->id_event?>"></a>
-                                    <span class="level"><?php echo $arItem[$idField]; ?></span>
-                                    <?php if (!empty($arItem[$nameField])) { ?>
-                                        <a href="<?=$singleUrl.$arItem[$keyField] ?>"><?php echo $arItem[$nameField]; ?></a>
-                                    <?php } else { ?>
-                                        <p>-</p>
-                                    <?php } ?>
-                                </dt>
-                            <?php } ?>
-                        </dl>
 
-                    <?php } else { ?>
-                        <p class="course-list-error">Елементы не найдены</p>
-                    <?php } ?>
-                </div> <!-- /.widget-inner -->
-            </div> <!-- /.widget-main -->
-        </div> <!-- /.col-md-12 -->
-    </div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="widget-main">
+				<div class="widget-inner">
+				<?php if (count($arResult) > 0) { ?>
+
+				<dl class="course-list" role="tablist">
+					<?php foreach($arResult as $arItem) { ?>
+				<dt>
+					<a class="fa fa-times pull-right" href="/event/delete/<? echo $arItem->id_event?>"></a>
+					<a class="fa fa-pencil pull-right" href="/event/update/<? echo $arItem->id_event?>"></a>
+					<span class="level"><?php echo $arItem->id_event ?></span>
+						<?php if (!empty($arItem->name_event)) { ?>
+						<a href="/event/single/<?=$arItem->id_event ?>"><?php echo $arItem->name_event; ?></a>
+						<?php } else { ?>
+					<span>-</span>
+					<?php } ?>
+				</dt>
+				<?php } ?>
+				</dl>
+
+<?php } else { ?>
+<p class="course-list-error">Р•Р»РµРјРµРЅС‚С‹ РЅРµ РЅР°Р№РґРµРЅС‹</p>
+<?php } ?>
+</div> <!-- widget-inner вЂ”>
+</div> <!-- /.widget-main вЂ”>
+</div> <!-- /.col-md-12 вЂ”>
+</div>
 </div>
