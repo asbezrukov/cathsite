a794187d43de8579f3f0db58fe7d9fdcc36181e4
SFomin116 <sergei.fomin@list.ru>
2014-05-30 13:42:38 +0600

    Добавлена кнопка "Скрыть" у админ. панели.

--- protected/views/admin/list_events.php
+++ protected/views/admin/list_events.php
@@ -20,28 +20,30 @@
 		<div class="col-md-12">
 			<div class="widget-main">
 				<div class="widget-inner">
-				<?php if (count($arResult) > 0) { ?>
 
-				<dl class="course-list" role="tablist">
-					<?php foreach($arResult as $arItem) { ?>
-				<dt>
-					<a class="fa fa-times pull-right" href="/event/delete/<? echo $arItem->id_event?>"></a>
-					<a class="fa fa-pencil pull-right" href="/event/update/<? echo $arItem->id_event?>"></a>
-					<span class="level"><?php echo $arItem->id_event ?></span>
-						<?php if (!empty($arItem->name_event)) { ?>
-						<a href="/event/single/<?=$arItem->id_event ?>"><?php echo $arItem->name_event; ?></a>
-						<?php } else { ?>
-					<span>-</span>
-					<?php } ?>
-				</dt>
-				<?php } ?>
-				</dl>
+                    <?php if (count($arResult) > 0) { ?>
 
-<?php } else { ?>
-<p class="course-list-error">Елементы не найдены</p>
-<?php } ?>
-</div> <!-- widget-inner —>
-</div> <!-- /.widget-main —>
-</div> <!-- /.col-md-12 —>
-</div>
+                        <dl class="course-list" role="tablist">
+                            <?php foreach($arResult as $arItem) { ?>
+                                <dt>
+                                    <a class="fa fa-times pull-right" href="/event/delete/<? echo $arItem->id_event?>"></a>
+                                    <a class="fa fa-pencil pull-right" href="/event/update/<? echo $arItem->id_event?>"></a>
+                                    <span class="level"><?php echo $arItem->id_event ?></span>
+                                        <?php if (!empty($arItem->name_event)) { ?>
+                                        <a href="/event/single/<?=$arItem->id_event ?>"><?php echo $arItem->name_event; ?></a>
+                                        <?php } else { ?>
+                                    <span>-</span>
+                                    <?php } ?>
+                                </dt>
+                            <?php } ?>
+                        </dl>
+
+                    <?php } else { ?>
+                        <p class="course-list-error">Елементы не найдены</p>
+                    <?php } ?>
+
+                </div> <!-- widget-inner -->
+            </div> <!-- /.widget-main -->
+        </div> <!-- /.col-md-12 -->
+    </div>
 </div>
--- protected/widgets/views/adminPanelForm.php
+++ protected/widgets/views/adminPanelForm.php
@@ -13,26 +13,29 @@
             font-size: 12px;
             font-weight: bold;
         }
-        .admin-panel a {
+        .admin-panel-body a {
             color: #8ab5d6;
             display: block;
             padding: 7px 30px;
             border-bottom: 1px solid #00569d;
         }
-            .admin-panel a.active {
+            .admin-panel-body a.active {
                 color: #fecd0b;
             }
-            .admin-panel a:hover {
+            .admin-panel-body a:hover {
                 background-color: #00569d;
                 text-decoration: none;
             }
-    .admin-panel-hidden .admin-panel-body {
+    .admin-panel-outer {
+        border-top: 1px solid #00569d;
+    }
+    .admin-panel-hidden .admin-panel-outer {
         overflow: hidden;
         white-space: nowrap;
         width: 5px;
     }
-    .admin-panel-body {
-        border-top: 1px solid #00569d;
+    .admin-panel-hidden .admin-panel-lock {
+        display: block;
     }
     [class*=admin-panel-fixed-] {
         position: fixed;
@@ -47,6 +50,28 @@
         right: 0;
         border-left: 4px solid #fecd0b;
     }
+    .admin-panel-actions {
+        display: none;
+        text-align: right;
+        position: absolute;
+        right: 0;
+        bottom: -20px;
+    }
+    .admin-panel-actions.admin-panel-actions-opened {
+        display: block;
+    }
+        .admin-panel-actions a {
+            padding-top: 5px;
+        }
+    .admin-panel-lock {
+        display: none;
+        position: absolute;
+        background-color: #004884;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
 </style>
 <script>
     $(document).ready(function() {
@@ -55,7 +80,9 @@
             if (elem.hasClass('admin-panel-hidden')) {
                 show_panel();
             }
+            show_actions();
         }, function() {
+            hide_actions();
         });
     })
     function show_panel() {
@@ -67,14 +94,28 @@
         var elem = $('.admin-panel');
         elem.addClass('admin-panel-hidden');
     }
+    function show_actions() {
+        var elem = $('.admin-panel-actions');
+        elem.addClass('admin-panel-actions-opened');
+    }
+    function hide_actions() {
+        var elem = $('.admin-panel-actions');
+        elem.removeClass('admin-panel-actions-opened');
+    }
 </script>
 
 <div class="admin-panel admin-panel-fixed-left">
-    <div class="admin-panel-body">
-        <p class="title">Редактировать:</p>
-        <?php foreach($arResult as $arItem) { ?>
-            <a class="<?php if ($arItem['active']) echo 'active'; ?>" href="<?php echo $arItem['url'] ?>"><?php echo $arItem['name'] ?></a>
-        <?php } ?>
-        <!--<a id="admin-panel-hide" href="#" onclick="hide_panel(); return false;">Скрыть</a>-->
+    <div class="admin-panel-outer">
+        <div class="admin-panel-body">
+            <p class="title">Редактировать:</p>
+            <?php foreach($arResult as $arItem) { ?>
+                <a class="<?php if ($arItem['active']) echo 'active'; ?>" href="<?php echo $arItem['url'] ?>"><?php echo $arItem['name'] ?></a>
+            <?php } ?>
+        </div>
+        <div class="admin-panel-actions">
+            <a id="admin-panel-hide" href="#" onclick="hide_panel(); return false;">&laquo;Скрыть</a>
+        </div>
+    </div>
+    <div class="admin-panel-lock">
     </div>
 </div>
